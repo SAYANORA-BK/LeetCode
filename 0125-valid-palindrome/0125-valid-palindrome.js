14f798daa89ffbd2dceaddaa27c11009ac6b45c9
SAYANORA-BK <sayanorabk14@gmail.com>
2024-12-17 09:54:17 +0530

    Time: 7 ms (36.75%), Space: 53.1 MB (49.83%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let str=s.toLowerCase().replace(/[^a-z0-9]/g,'');
+    return str === str.split('').reverse().join('');
+    
+};
