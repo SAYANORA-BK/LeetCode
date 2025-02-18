258ec0f68aac05d9da7ddfa17cd690fbf0074bab
Sayanora Bk <sayanorabk14@gmail.com>
2025-02-18 09:47:19 +0530

    Time: 19 ms (23.17%), Space: 52.3 MB (89.56%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+     if (x < 2) {
+        return x;
+    }
+
+    let i = 1;
+    while (i * i <= x) {
+        i++;
+    }
+    return i - 1;
+    
+};
