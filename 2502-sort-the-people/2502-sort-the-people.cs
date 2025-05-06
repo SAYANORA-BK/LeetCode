9bed8e2d3677825992970c880f037acf9992cf84
Sayanora Bk <sayanorabk14@gmail.com>
2025-05-06 18:22:48 +0530

    Time: 8 ms (88.89%), Space: 63.9 MB (27.35%) - LeetHub

---
+++ 2502-sort-the-people/2502-sort-the-people.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public string[] SortPeople(string[] names, int[] heights) {
+        Array.Sort(heights,names);
+        var name=names.ToList<string>();
+        name.Reverse();
+        return name.ToArray();
+    }
+}
