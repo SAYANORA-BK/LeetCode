a090657510a14d70d1ca72e76377a1107b9be0e3
Sayanora Bk <sayanorabk14@gmail.com>
2025-05-07 18:38:20 +0530

    Time: 1 ms (6.78%), Space: 43.3 MB (28.88%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public int SearchInsert(int[] nums, int target) {
+        var index = Array.BinarySearch(nums, target);
+        return index >= 0 ?  index : ~index;
+        
+    }
+}
