ee51e7d4cc5290db27a059c03ddc643440bf6b34
Sayanora Bk <sayanorabk14@gmail.com>
2025-05-13 17:57:10 +0530

    Time: 13 ms (70.97%), Space: 63.3 MB (61.16%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cs
@@ -0,0 +1,8 @@
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        
+        HashSet<int> set = new HashSet<int>(nums);
+        
+        return nums.Length != set.Count;
+    }
+}
